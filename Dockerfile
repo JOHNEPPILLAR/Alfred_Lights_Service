f88ae955906ba59a37eca7fcb3275da576cba52b
JOHNEPPILLAR <johneppillar@gmail.com>
2018-05-10 14:56:15 +0100

    Updated ports

--- Dockerfile
+++ Dockerfile
@@ -16,4 +16,4 @@ COPY . /home/nodejs/app
 
 CMD [ "pm2-runtime", "start", "/home/nodejs/app/pm2.json" ]
 
-EXPOSE 3992
+EXPOSE 3982
--- updateDocker.sh
+++ updateDocker.sh
@@ -7,4 +7,4 @@ docker rmi alfred_lights_service
 
 echo "Build & run new ver"
 sudo docker build --no-cache --rm=true -t alfred_lights_service .
-docker run -d --restart=always --name alfred_lights_service -p 3992:3992 alfred_lights_service
+docker run -d --restart=always --name alfred_lights_service -p 3982:3982 alfred_lights_service
