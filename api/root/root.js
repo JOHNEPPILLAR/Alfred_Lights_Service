290345874c3509dd3ecb3f9e455df518933d2c2d
JOHNEPPILLAR <johneppillar@gmail.com>
2018-05-24 06:38:59 +0100

    Added a re register end point

--- api/root/root.js
+++ api/root/root.js
@@ -41,4 +41,38 @@ function ping(req, res, next) {
 }
 skill.get('/ping', ping);
 
+/**
+ * @api {get} /reregister
+ * @apiName reregister
+ * @apiGroup Root
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *   HTTPS/1.1 200 OK
+ *   {
+ *     sucess: 'true'
+ *     data: {
+ *       sucess or filure return message
+ *     }
+ *   }
+ *
+ * @apiErrorExample {json} Error-Response:
+ *   HTTPS/1.1 400 Bad Request
+ *   {
+ *     data: Error message
+ *   }
+ *
+ */
+async function reRegister(req, res, next) {
+  serviceHelper.log('trace', 'reRegister', 'reRegister API called');
+
+  let returnMessage = 'Re-registered service';
+
+  if (!serviceHelper.registerService()) returnMessage = 'Unable to re-register service';
+
+  serviceHelper.log('trace', 'reRegister', returnMessage);
+  serviceHelper.sendResponse(res, false, returnMessage);
+  next();
+}
+skill.get('/reregister', reRegister);
+
 module.exports = skill;
