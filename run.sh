fb0122a06f5bd39913106ce65f7580d614577141
JOHNEPPILLAR <johneppillar@gmail.com>
2019-06-17 22:12:38 +0100

    Updated elk npm

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ lsof -i :3978
 kill -9 $(lsof -sTCP:LISTEN -i:3978 -t)
 
 #echo "Removing node modules folder and installing latest"
-#rm -rf node_modules
+rm -rf node_modules
 ncu -u
 npm update
 npm install
 
 echo "Run the server"
-npm run dev
+npm run test
