d4c26841e77d8d8a0ab0010e2cba661ba722ee69
JOHNEPPILLAR <johneppillar@gmail.com>
2019-09-27 19:59:07 +0100

    removed serviceName

--- api/root/root.js
+++ api/root/root.js
@@ -29,7 +29,7 @@ const skill = new Skills();
  *
  */
 async function ping(req, res, next) {
-  serviceHelper.ping(res, next, process.env.ServiceName);
+  serviceHelper.ping(res, next);
 }
 skill.get('/ping', ping);
 
